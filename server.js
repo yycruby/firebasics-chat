7699e0aaa52d9a2534c24629b1ec897196ce5a49
Tim Uruski <github@tmu.pw>
2016-03-08 16:19:34 -0700

    Remove livereload from server.js

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var serveStatic = require('serve-static');
-var livereload = require('livereload');
 
 var PORT = process.env.PORT || 3000;
 var app = express();
-
-var livereloadServer = livereload.createServer({
-  exts: ['html', 'css', 'js']
-});
-
-livereloadServer.watch(__dirname + '/public');
 
 app.use(serveStatic(__dirname + '/public'));
 app.listen(PORT);
